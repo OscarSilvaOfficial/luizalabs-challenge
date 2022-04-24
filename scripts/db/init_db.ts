05cfa99a1e846311d6d266c6524f8175b1c7a4ff
OscarSilvaOfficial <OscarSilvaOfficial>
2022-04-24 17:34:08 -0300

    Dados iniciáis do banco

---
+++ scripts/db/init_db.ts
@@ -0,0 +1,35 @@
+import {
+  MONGO_DATABASE_NAME,
+  MONGO_URL,
+} from '../../src/infra/configs/enviroment';
+import data from './init_db.json';
+import * as mongoDB from 'mongodb';
+
+export async function connectToDatabase() {
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGO_URL);
+
+  await client.connect();
+
+  const db: mongoDB.Db = client.db(MONGO_DATABASE_NAME);
+
+  const collection: mongoDB.Collection = db.collection('persons');
+
+  console.log(
+    `Successfully connected to database: ${db.databaseName} and collection: ${collection.collectionName}`,
+  );
+
+  return collection;
+}
+
+const insertDataOnDB = async () => {
+  const collection = await connectToDatabase();
+  const collectionData = await collection.find().toArray();
+  if (collectionData.length === 0) {
+    collection.insertMany(data);
+    console.log(
+      `Data inserted successfully to collection: ${collection.collectionName}`,
+    );
+  }
+};
+
+insertDataOnDB();
