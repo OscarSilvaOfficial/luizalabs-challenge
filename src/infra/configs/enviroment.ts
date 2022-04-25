20780d44ced50060f4bbf046db303c8ea55fb5bb
OscarSilvaOfficial <OscarSilvaOfficial>
2022-04-25 01:48:49 -0300

    Correção na string

--- src/infra/configs/enviroment.ts
+++ src/infra/configs/enviroment.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const MONGO_URL =
-  process.env.MONGO_URL || 'mongodb://root:root@localhost:27017';
+  process.env.MONGO_URL || 'mongodb://test:test@localhost:27017';
 
 export const MONGO_DATABASE_NAME = process.env.MONGO_DATABASE_NAME || 'test';
